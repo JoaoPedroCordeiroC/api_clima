6e96f147136427be7f12cc31705332b12c0f8e92
João Cordeiro <joao.u@hotmail.com>
2019-06-14 22:23:49 -0300

    iniciando retorno de temperatura

--- app/services/comunicacao_weather.rb
+++ app/services/comunicacao_weather.rb
@@ -4,12 +4,12 @@ class ComunicacaoWeather
 
         @cidade = cidade
 
-        url = "https://api.openweathermap.org/data/2.5/weather?q=#{@cidade},BR&APPID=b3338227dd5c58654213649658c0d76e"
+        url = "https://api.openweathermap.org/data/2.5/weather?q=#{@cidade},BR&APPID=b3338227dd5c58654213649658c0d76e&units=metric"
 
         retorno = JSON.parse(Net::HTTP.get(URI(url)))
 
         cidade = {
-            
+            "temperatura": retorno["main"]["temp"]
         }
     end
 end
