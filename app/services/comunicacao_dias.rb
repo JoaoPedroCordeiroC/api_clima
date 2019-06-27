e9aefbd9ec12cbcf42e1f361bb6b5d2d2e98d4a3
João Cordeiro <joao.u@hotmail.com>
2019-06-27 19:59:32 -0300

    Criando classe comunicacao_dias

--- app/services/comunicacao_dias.rb
+++ app/services/comunicacao_dias.rb
@@ -1,12 +1,13 @@
-class ComunicacaoHoras
+class BuscaDias
     require 'net/http'
     require 'json' 
     require 'time'
 
-    def buscar_horas(cidade)
+    def buscar_CincoDias(cidade)
+
         @cidade = cidade
 
-        url = "http://api.openweathermap.org/data/2.5/forecast?q=#{@cidade},BR&APPID=b3338227dd5c58654213649658c0d76e&units=metric"
+        url = "http://api.openweathermap.org/data/2.5/forecast?q=#{@cidade},BR&APPID=7fe5a0bcd8c43fe22e389436e4f73f1f"
     
         retorno = JSON.parse(Net::HTTP.get(URI(url)))
 
@@ -15,10 +16,10 @@ class ComunicacaoHoras
         retorno["list"].each do |horario|
             resposta << {Hora: Time.at(horario["dt"]),
             Clima: horario["clouds"]["all"],
+            Descrição: horario["weather"][0]["description"],
             Temperatura: horario["main"]["temp"],
             Temperatura_Minima: horario["main"]["temp_min"],
             Temperatura_Maxima: horario["main"]["temp_max"],
-            Descrição: horario["weather"][0]["description"],
             Pressão: horario["main"]["pressure"],
             Humidade: horario["main"]["humidity"],
             Vento: horario["wind"]["speed"]
@@ -26,6 +27,7 @@ class ComunicacaoHoras
         end
 
         resposta
+
 
     end
 
